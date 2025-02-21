2df41b2f5bd05b257ac6608f1a48d37c94425b8b
fany-santos8 <estefany_fanysantos@hotmail.com>
2025-02-21 17:09:00 -0300

    Enviando atualizaçoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import login from "./components/login/Login.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login.jsx";
+import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
+import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
+import Analise from "./components/analise/Analise.jsx";
+import Historico from "./components/historico/Historico.jsx";
+import "./global.scss";
+
 function App() {
   return (
-    <>
-      <login/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reembolsos" element={<Reembolsos />} />
+        <Route path="/solicitacao" element={<Solicitacao />} />
+        <Route path="/analise" element={<Analise />} />
+        <Route path="/historico" element={<Historico />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,44 +1,59 @@
-import "react";
-//import Capa from "../../assets/Tela de Login/imagem tela de login.png"
-import Logo from "../../assets/Tela de Login/logo-ws.png"
+//import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/Tela-Login/logo-ws.png'
+import style from './login.module.scss'
 
-function Login() {
-  return (
-    <main>
-      <section id="background-navio">
-      <p>Reservado para a imagem</p>
-      </section>
+export default function Login() {
+    const navigate = useNavigate();
 
-      <section>
-        <img src={Logo} alt="Logo" />
-        <div id="container-titler">
-        <h1>Boas Vindas ao Novo Portal SISPAR</h1>
-        <p>Sistema de Emissão de Boletos e Parcelamento</p>
-        </div>
-        <form action="#">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email"
-          ></input>
-          <input
-            type="senha"
-            name="senha"
-            id="senha"
-            placeholder="Senha"
-          ></input>
+    const handleLogin = () => {
+        navigate('/reembolsos');
+    }
 
-          <a href="#" rel="noopener noreferrer">Esqueci minha senha</a>
-
-          <div id="button-container">
-          <button id="entrar">Entrar</button>
-          <button>Criar conta</button>
-          </div>
-        </form>
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <section id={style['backgroundNavio']}>
+            </section>
+            <section className={style.sectionLogin}>
+                <article>
+                    <img src={logo} alt="Logo da página" />
+                    <div id={style['containerTitle']}>
+                        <h1>
+                            Boas vindas ao Novo Portal SISPAR
+                        </h1>
+                        <p>
+                            Sistema de Emissão de Boletos e Parcelamento
+                        </p>
+                    </div>
+                    <form action="#">
+                        <input
+                            type="email"
+                            name="email"
+                            id={style['email']}
+                            placeholder="Email"
+                            required
+                        />
+                        <input
+                            type="password"
+                            name="senha"
+                            id={style['senha']}
+                            placeholder="Senha"
+                            required
+                        />
+                        <a href="#" rel="noopener noreferrer">
+                            Esqueci minha senha
+                        </a>
+                        <div id={style["buttonContainer"]}>
+                            <button id={style['entrar']} onClick={handleLogin}>
+                                Entrar
+                            </button>
+                            <button id={style['criar']}>
+                                Criar conta
+                            </button>
+                        </div>
+                    </form>
+                </article>
+            </section>
+        </main>
+    )
 }
-
-export default Login;
